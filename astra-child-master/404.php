330bc2c5b65c963e33a208498cc3674cddb67e03
Xiaolei Bi And. <xbi@eamv.dk>
2026-06-18 13:01:44 +0200

    Add files via upload

---
+++ astra-child-master/404.php
@@ -0,0 +1,18 @@
+<?php
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
+		<section class="error-404 not-found">
+			<header class="page-header">
+				<h1 class="page-title">404</h1>
+			</header>
+			<div class="page-content">
+				<p>Det ser ud til, at du er kommet forkert. Siden, du leder efter, findes ikke.</p>
+				<?php get_search_form(); ?>
+			</div>
+		</section>
+	</main>
+</div>
+
+<?php get_footer(); ?>
---
+++ astra-child-master/functions.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Astra Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra-child
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'ASTRA_CHILD_THEME_VERSION', '1.0.1' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), ASTRA_CHILD_THEME_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+  
+add_filter( 'gettext', function( $translated, $text, $domain ) {
+    if ( 'astra' === $domain && "This page doesn't seem to exist." === $text ) {
+        return 'Det ser ud til, at linket til denne side ikke virker korrekt.';
+    }
+    return $translated;
+}, 20, 3 );
+
+add_filter( 'astra_default_strings', function( $strings ) {
+    $strings['string-404-sub-title'] = 'Det ser ud til, at linket, der førte dig hertil, ikke virker. Måske kan du prøve at søge i stedet?';
+    return $strings;
+} );
+
+/* force mobile menu to off-canvas (flyout) from left */
+add_filter( 'astra_get_option_mobile-header-type', function( $value ) {
+    return 'off-canvas';
+} );
+
+add_filter( 'astra_get_option_off-canvas-slide', function( $value ) {
+    return 'right';
+} );
